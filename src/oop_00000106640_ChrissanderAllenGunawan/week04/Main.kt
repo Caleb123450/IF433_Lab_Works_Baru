4f25e4d1faa3f8f56d16e25787d7f190736ca1ad
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-04-25 13:09:54 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000106640_ChrissanderAllenGunawan/week04/Main.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week04/Main.kt
@@ -17,4 +17,14 @@ fun main() {
     myTesla.accelerate()
     myTesla.honk()
     myTesla.openTrunk()
+
+    println("\n--- Testing Employee Hierarchy ---")
+
+    val manager = Manager(name = "Budi", baseSalary = 10000000)
+    manager.work()
+    println("Bonus ${manager.name}: Rp ${manager.calculateBonus()}")
+
+    val developer = Developer(name = "Ani", baseSalary = 8000000, programmingLanguage = "Kotlin")
+    developer.work()
+    println("Bonus ${developer.name}: Rp ${developer.calculateBonus()}")
 }
