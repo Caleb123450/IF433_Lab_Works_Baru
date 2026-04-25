c7bc625759d94edbb61b7482674ed9e0441422af
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-04-25 22:14:29 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_00000106640_ChrissanderAllenGunawan/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_00000106640_ChrissanderAllenGunawan.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}
