b519df5a513debabb700f983fddff90c306afadf
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-04-25 09:34:24 +0700

    week01: refactor to use val and string templates

--- src/oop_00000106640_ChrissanderAllenGunawan/week01/GradingSystem.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week01/GradingSystem.kt
@@ -1,7 +1,7 @@
 package oop_00000106640_ChrissanderAllenGunawan.week01
 
 fun main(args: Array<String>) {
-    var studentName: String = "John"
-    var score: Int = 85
-    println("Student: " + studentName + ", Score: " + score)
+    val studentName = "John"
+    val score = 85
+    println("Student: $studentName, Score: $score")
 }
