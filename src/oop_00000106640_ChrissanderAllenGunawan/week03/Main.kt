86b02eed265d866f8b281087414577d26b5e4dbb
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-04-25 12:58:05 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000106640_ChrissanderAllenGunawan/week03/Main.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week03/Main.kt
@@ -14,4 +14,18 @@ fun main() {
 
     // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n=== TEST WEAPON ===")
+    val sword = Weapon("Excalibur")
+
+    // Test damage negatif (harus gagal)
+    sword.damage = -50
+    println("Damage setelah set -50: ${sword.damage}") // Harusnya tetap 0
+
+    // Test damage overpowered (harus dipaksa jadi 1000)
+    sword.damage = 9999
+    println("Damage setelah set 9999: ${sword.damage}") // Harusnya jadi 1000
+
+    // Test tier
+    println("Tier: ${sword.tier}") // Harusnya "Legendary"
 }
