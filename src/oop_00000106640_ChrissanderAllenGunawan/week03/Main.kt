abe585621ef96115d2c7e4a448f5ec8959367e8e
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-04-25 12:53:16 +0700

    week03: fix setter using backing field

--- src/oop_00000106640_ChrissanderAllenGunawan/week03/Main.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week03/Main.kt
@@ -2,6 +2,9 @@ package oop_00000106640_ChrissanderAllenGunawan.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000 // Ini akan meledakkan program Anda
+
+    // 1. Test Validasi Salary
+    e.salary = -1000 // Harusnya print Error
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
 }
