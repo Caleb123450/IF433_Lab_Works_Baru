53b621dcf02e605bd79a0c150f54fd13cd462972
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-04-25 22:20:40 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000106640_ChrissanderAllenGunawan/week07/Main.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week07/Main.kt
@@ -1,6 +1,11 @@
 package oop_00000106640_ChrissanderAllenGunawan.week07
 
 fun main() {
-    // ERROR: Constructor is private
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()  // Instansiasi lewat Factory
+    client.connect()
 }
