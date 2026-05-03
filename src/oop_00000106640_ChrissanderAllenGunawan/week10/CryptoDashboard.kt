98f530bb82ba6e9ba79aea85b5c9e60beab3a8b4
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-03 11:52:55 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000106640_ChrissanderAllenGunawan/week10/CryptoDashboard.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week10/CryptoDashboard.kt
@@ -6,5 +6,11 @@ fun main() {
     coinRepo.add(Coin("ETH", 3.2))
     coinRepo.add(Coin("USDT", 1500.0))
 
-    val response = ApiResponse("200 OK", coinRepo.getAll()
+    val response = ApiResponse("200 OK", coinRepo.getAll())
+
+    println("Status: ${response.status}")
+    println("\n=== CRYPTO WALLET DASHBOARD ===")
+    response.data.forEach { coin ->
+        println("Koin: ${coin.name} | Balance: ${coin.balance}")
+    }
 }
