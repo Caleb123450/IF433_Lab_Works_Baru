5cc22f7145b63f10489d69e192b105b237e4b5ab
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-03 11:53:31 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000106640_ChrissanderAllenGunawan/week10/CryptoDashboard.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week10/CryptoDashboard.kt
@@ -12,5 +12,15 @@ fun main() {
     println("\n=== CRYPTO WALLET DASHBOARD ===")
     response.data.forEach { coin ->
         println("Koin: ${coin.name} | Balance: ${coin.balance}")
+
+        val txRepo = WalletRepository<Transaction>()
+        txRepo.add(Transaction("TX001", 250.0))
+        txRepo.add(Transaction("TX002", -50.0))
+        txRepo.add(Transaction("TX003", 1000.0))
+
+        println("\n=== TRANSACTION HISTORY ===")
+        txRepo.getAll().forEach { tx ->
+            println("ID: ${tx.id} | Amount: ${tx.amount}")
+        }
     }
 }
