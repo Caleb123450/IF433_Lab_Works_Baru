b7a230187a13103e9790c2b6cb64f6419d43acd6
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-03 10:58:58 +0700

    week10: implement generic function with return type

--- src/oop_00000106640_ChrissanderAllenGunawan/week10/GenericMethods.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week10/GenericMethods.kt
@@ -2,4 +2,8 @@ package oop_00000106640_ChrissanderAllenGunawan.week10
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+
+    fun <T> processData(input: T): T {
+        return input
+    }
 }
