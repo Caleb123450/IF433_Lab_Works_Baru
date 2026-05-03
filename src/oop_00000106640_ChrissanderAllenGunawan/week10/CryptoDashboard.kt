838071d5abdfb59f0191f3cfb7004b3e457a9d97
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-03 11:49:23 +0700

    week10: (task) populate coin repository

--- src/oop_00000106640_ChrissanderAllenGunawan/week10/CryptoDashboard.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000106640_ChrissanderAllenGunawan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 3.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
