f6826f5dcfd591ea53d48e18928aad9b0dc2a311
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-06 10:19:38 +0700

    week11: (task) calculate total power usage

--- src/oop_00000106640_ChrissanderAllenGunawan/week11/SmartHomeDashboard.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week11/SmartHomeDashboard.kt
@@ -31,4 +31,6 @@ fun main() {
         println("\n=== SMART HOME SUMMARY ===")
         println("Total perangkat terdaftar: ${this.size}")
     }
+    val totalPower = homeDevices.run { sumOf { it.powerLoad } }
+    println("Total Daya: $totalPower Watt")
 }
