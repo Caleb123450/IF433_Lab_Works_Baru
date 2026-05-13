f47922340b435a66b9614c01ee87a13a3a8a58ed
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-13 10:27:50 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
@@ -33,4 +33,8 @@ fun main() {
     }finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    println("\n=== JADWAL MAKAN SORE ===")
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
