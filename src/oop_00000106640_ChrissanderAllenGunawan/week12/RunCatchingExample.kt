d4b2f600e60cdd53edfd20a32f5dead57e8b5b9b
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-13 10:03:37 +0700

    week12: implement basic runCatching

---
+++ src/oop_00000106640_ChrissanderAllenGunawan/week12/RunCatchingExample.kt
@@ -0,0 +1,7 @@
+package oop_00000106640_ChrissanderAllenGunawan.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
