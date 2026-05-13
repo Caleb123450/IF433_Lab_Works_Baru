ed2188bb796cd7545f7ee2a0c5f07aa9c88915d6
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-13 10:19:02 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
@@ -3,11 +3,14 @@ package oop_00000106640_ChrissanderAllenGunawan.week12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
-    fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
-        require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-
-        if (isJammed) {
-            throw DispenserJamException()
-        }
+    if (isJammed) {
+        throw DispenserJamException()
     }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
