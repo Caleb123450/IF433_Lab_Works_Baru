b22d34cbcebc5a8915521f5d4d93af235f9a8668
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-13 10:18:20 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
+++ src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
@@ -2,4 +2,12 @@ package oop_00000106640_ChrissanderAllenGunawan.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+        require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+        if (isJammed) {
+            throw DispenserJamException()
+        }
+    }
 }
