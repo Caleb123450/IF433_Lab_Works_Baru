bd5458756cd1bc0af5e7dc5e10ee07170f33011b
Chrissander Alien Gunawan <chrissander.gunawan@student.umn.ac.id>
2026-05-13 10:12:56 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000106640_ChrissanderAllenGunawan/week12/SmartFeeder.kt
@@ -0,0 +1,5 @@
+package oop_00000106640_ChrissanderAllenGunawan.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
